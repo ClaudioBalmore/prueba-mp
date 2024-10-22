b9816ebc37a947dc202d757c4f4ecce85db58782
Claudio Nunez <cnunezcerda@gmail.com>
2024-10-22 08:06:31 -0300

    pruebamp

--- index.php
+++ index.php
@@ -16,3 +16,16 @@ fwrite($file, "NOTIFICACIÓN: " . $datos . PHP_EOL);
 fclose($file);
 
 http_response_code(200);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Notificacion</title>
+</head>
+<body>
+    <h1>Prueba Correcta!!</h1>
+</body>
+</html>
