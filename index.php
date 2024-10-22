030c515b982dc1e099ff61211a438c7ecf6b4c61
Claudio Nunez <cnunezcerda@gmail.com>
2024-10-22 10:19:29 -0300

    prueba correo

--- index.php
+++ index.php
@@ -5,10 +5,11 @@ use MercadoPago\MercadoPagoConfig;
 
 require 'vendor/autoload.php';
 
+$resend = Resend::client('re_Qc72Wbin_CFgyECUTvXbPLw4NPXfSaVwF');
+
 MercadoPagoConfig::setAccessToken("APP_USR-7814564292133896-100119-aa29a73b031435648377a718733848d2-2016921642");
 
 $notificacion = file_get_contents("php://input");
-
 $datos = json_decode($notificacion, true);
 
 if (isset($datos['type']) && $datos['type'] == 'payment') {
@@ -19,15 +20,20 @@ if (isset($datos['type']) && $datos['type'] == 'payment') {
     try {
 
         $payment = $paymentClient->get($paymentId);
-        echo "<pre>";
-        echo var_dump($payment);
-        echo "</pre>";
-
         $filePath = "pagos.txt";
         $file = fopen($filePath, 'a');
         fwrite($file, "NOTIFICACIÓN: " . json_encode($datos, JSON_PRETTY_PRINT) . PHP_EOL);
         fwrite($file, "PAGO: " . json_encode($payment, JSON_PRETTY_PRINT) . PHP_EOL);
         fclose($file);
+
+        $result = $resend->emails->send([
+            'from' => 'Contacto <onboarding@resend.dev>',
+            'to' => ['cnunezcerda@gmail.com'],
+            'subject' => 'Notificación de Compra',
+            'html' => var_dump($payment)
+        ]);
+
+
 
     } catch (Exception $e) {
         error_log($e->getMessage());
@@ -49,6 +55,6 @@ http_response_code(200);
 </head>
 <body>
     <h1>Json Correcto!!</h1>
-    
+
 </body>
 </html>
